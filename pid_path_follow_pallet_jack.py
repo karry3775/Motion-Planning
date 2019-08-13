c8c825f279e7d9542e64f24fb4e89cca3b423eeb
kartik <kartikprakash3775@gmail.com>
2019-08-13 18:40:23 -0500

    improved pid based path following

--- pid_path_follow_pallet_jack.py
+++ pid_path_follow_pallet_jack.py
@@ -21,6 +21,8 @@ def wrapToPi(theta):
     return m.atan2(m.sin(theta),m.cos(theta))
 
 def calc_perp(x,y,pt1,pt2):
+    #this also needs to tell me if the xt,yt point has gone beyond pt2
+    skip = False
     ld = 1
     #find the equation of the line
     x1,y1 = pt1
@@ -33,6 +35,13 @@ def calc_perp(x,y,pt1,pt2):
         yp = intercept + slope*xp
         xt = xp + m.cos(phi)
         yt = yp + m.sin(phi)
+        #checking if it has crossed pt2
+        check_angle = m.atan2((yt-y2),(xt-x2))
+        if check_angle == phi:
+            skip = True
+        else:
+            #everything is fine
+            pass
 
     except ZeroDivisionError:
         xp = x1
@@ -40,7 +49,14 @@ def calc_perp(x,y,pt1,pt2):
         sign = (y2-y1)/abs(y2-y1)
         xt = xp
         yt = yp + sign*1
-    return(xp,yp,xt,yt)
+        #here also we need to check the crossing thresh
+        if sign>0:
+            if yt>y2:
+                skip = True
+        elif yt<y2:
+            skip = True
+
+    return(xp,yp,xt,yt,skip)
 
 def calc_target(x,y,goal_points):
     if len(goal_points) >=3:
@@ -54,7 +70,7 @@ def calc_target(x,y,goal_points):
     for i in range(len(goal_points)-1):
         pt1 = goal_points[i]
         pt2 = goal_points[i+1]
-        xp,yp,xt,yt = calc_perp(x,y,pt1,pt2)
+        xp,yp,xt,yt,skip = calc_perp(x,y,pt1,pt2)
         perp_dist = m.sqrt((x-xp)**2 + (y-yp)**2)
         if perp_dist < best_dist:
             best_dist = perp_dist
@@ -62,9 +78,9 @@ def calc_target(x,y,goal_points):
             next_pt = goal_points[i+1]
             best_target[0] =xt
             best_target[1] =yt
-            proximity = m.sqrt((next_pt[0]-xt)**2 + (next_pt[1]-yt)**2)
+            proximity = m.sqrt((next_pt[0]-xt)**2 + (next_pt[1]-yt)**2) #to pt2
 
-    return (best_target,proximity)
+    return (best_target,proximity,skip)
 
 def seek_one(start,goal):
     x,y,theta = start
@@ -74,7 +90,7 @@ def seek_one(start,goal):
     beta = m.atan2((goal[1]-y),(goal[0]-x))
     dist_error = m.sqrt((x-goal[0])**2 + (y-goal[1])**2)
     heading_error = wrapToPi(beta - theta)
-    print(m.degrees(heading_error),m.degrees(beta))
+    # print(m.degrees(heading_error),m.degrees(beta))
     s = Kp*heading_error
     if s > m.pi/2:
         s = m.pi/2
@@ -93,7 +109,11 @@ def seek_one(start,goal):
 # start = [0,0,0]
 # goal_points = [[1,2],[2,4],[3,5],[4,7],[5,8]] # successfull path follow
 start = [-2,-2,0]
-goal_points = [[0,0],[1.98,2.375],[4.04,4.23],[6.28,5],[8.75,4.079],[10.51,2.45],[12,0],[14,-2]]
+goal_points = [[0,0],[1.98,2.375],[4.04,4.23],[6.28,5],[8.75,4.079],[10.51,2.45],[12,0],[14,-2]] #succesfull
+# start = [0,0,0]
+# goal_points = [[0,0],[2,2],[6,7],[9,10]] #succesfull
+# start = [-2,-2,0]
+# goal_points = [[0,0],[1,2],[1,-5],[5,6]]
 dummy_gp = copy.deepcopy(goal_points)
 goal = [15,9]
 # goal_points = [[2,2]]
@@ -125,18 +145,20 @@ y_traj = []
 #         plt.plot(x_traj,y_traj,'--')
 #         x,y,theta,s = seek_one([x,y,theta],[xg,yg])
 #         plt.pause(0.0001)
-
+skip = False
 while len(dummy_gp) >1:
     #first step would be to find the target point
-    target,proximity = calc_target(x,y,dummy_gp)
+    target,proximity,skip = calc_target(x,y,dummy_gp)
     xt,yt = target
-    if proximity<0.1:
+    if proximity<0.1 or skip==True:
         dummy_gp.pop(0)
+    if skip==True:
+        print(skip)
     plt.cla()
     plt.axis('scaled')
     plt.xlim(-5,15)
     plt.ylim(-5,15)
-    plt.plot(gp_array[:,0],gp_array[:,1])
+    # plt.plot(gp_array[:,0],gp_array[:,1])
     plt.plot(start[0],start[1],'co')
     plt.plot(xt,yt,'ro')
 
